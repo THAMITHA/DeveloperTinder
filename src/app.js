504efddb5d88f2b23a3ddc4c0b552cf9c0370bc9
Thamitha <thamitharajput@gmail.com>
2025-06-28 07:39:20 +0530

    wildcard-error-handling

--- src/app.js
+++ src/app.js
@@ -1,29 +1,22 @@
 const express = require('express');
-const {adminAuth, userAuth} = require('./middlewares/auth');
 
-
-//instance of express server
 const app = express()
 
-//Handle Auth Middleware for all GET POST, .... requests
-app.use("/admin", adminAuth)
-
-// get login page no need for user to authorize
-app.use("/user/login", (req,res)=>{
-    res.send("User logged in successfully!")
+app.get("/getUserData", (req, res)=>{
+    // Logic to handle DB call and get user data
+    throw new Error("dfgfdgdfgdfg")
+    res.send("User Data Sent")
 })
 
-app.get("/user/data", userAuth, (req, res)=>{
-    res.send("User Data Sent");
+app.use("/",(err, req, res, next)=>{
+    if(err){
+        res.status(500).send("something went wrong")
+    }
+    else{
+        next()
+    }
 })
 
-app.get("/admin/getAllData", (req, res)=>{
-    res.send("All Data Sent")
-})
-
-app.delete("/admin/deleteUser", (req, res)=>{
-    res.send("Deleted a user")
-})
 //listening on port
 app.listen(3000, ()=>{
     console.log("Server is successfully listening on port 3000...")
